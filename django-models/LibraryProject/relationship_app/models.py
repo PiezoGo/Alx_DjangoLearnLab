74a783561d79a9da6e0aad22ee27afdae1f57370
PiezoGo <Piezo5go@gmail.com>
2025-11-09 20:59:39 +0300

    trial 2

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -14,6 +14,7 @@ class Book(models.Model):
     def __str__(self):
         return self.title
 
+#library
 class Library(models.Model):
     name = models.CharField(max_length=100)
     books = models.ManyToManyField(Book)
