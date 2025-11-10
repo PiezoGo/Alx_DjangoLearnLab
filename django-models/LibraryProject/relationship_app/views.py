4631c6d35b26b42c806ac4eae875b85d42fd332b
PiezoGo <Piezo5go@gmail.com>
2025-11-10 10:34:07 +0300

    I think everything is okay in task 1

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from .models import Book
-from django.views.generic import DetailView
+from django.views.generic.detail import DetailView
 
 
 # relationship_app/list_books.html
