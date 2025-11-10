ed23c6b2946a552f4fa67e0c8f0d43ae329eb7d4
PiezoGo <Piezo5go@gmail.com>
2025-11-10 11:27:56 +0300

    I think everything is okay in task 2

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import list_books, LibraryDetailView,LoginView,LogoutView
+from .views import list_books,LibraryDetailView,LoginView,LogoutView
 
 urlpatterns = [
    path('login/', LoginView.as_view(template_name='registration/login.html'), name='login'),
    path('logout/', LogoutView.as_view(template_name='logout.html'), name='logout'),
+   path('register/', views.register, name='register'),
    path('',  list_books, name='library'),
    path('detail/<int:pk>', LibraryDetailView.as_view(), name='detail')
 
