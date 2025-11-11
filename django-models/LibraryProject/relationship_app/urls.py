22c91571118cf8cd692826acd6021d6dddc21a65
PiezoGo <Piezo5go@gmail.com>
2025-11-11 06:54:42 +0300

    Final

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -11,5 +11,8 @@ urlpatterns = [
    path('admin-view/', Admin.as_view(), name='admin_view'),
    path('librarian-view/', Librarian.as_view(), name='librarian_view'),
    path('member-view/', Member.as_view(), name='member_view'),
+   path('add_book/', views.add_book, name='add_book'),
+   path('edit_book/<int:book_id>/', views.edit_book, name='edit_book'),
+   path('delete_book/<int:book_id>/', views.delete_book, name='delete_book'),
 
 ]
