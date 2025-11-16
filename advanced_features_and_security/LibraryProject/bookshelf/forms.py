d96775b3e31109ed7ea17d60520f0670586a8013
PiezoGo <Piezo5go@gmail.com>
2025-11-16 21:28:33 +0300

    trial2

---
+++ advanced_features_and_security/LibraryProject/bookshelf/forms.py
@@ -0,0 +1,5 @@
+from django.contrib.auth.models import AbstractUser
+
+class ExampleForm(AbstractUser):
+    date_of_birth = models.DateField(null=True, blank=True)
+    profile_photo = models.ImageField(upload_to='profile_photos/', null=True, blank=True)
