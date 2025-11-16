603d89da880fd89a54ba4a4f87183f34c83b3ca2
PiezoGo <Piezo5go@gmail.com>
2025-11-16 21:19:57 +0300

    new

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -5,9 +5,9 @@ from django.contrib.auth.decorators import permission_required
 from .models import Book
 
 @permission_required('books.can_view', raise_exception=True)
-def list_books(request):
+def book_lists(request):
     books = Book.objects.all()
-    return render(request, "books/list_books.html", {"books": books})
+    return render(request, "books/book_lists.html", {"books": books})
 
 @permission_required('books.can_create', raise_exception=True)
 def create_book(request):
@@ -16,7 +16,7 @@ def create_book(request):
             title=request.POST.get("title"),
             author=request.POST.get("author"),
         )
-        return redirect("list_books")
+        return redirect("book_lists")
     return render(request, "books/create_book.html")
 
 @permission_required('books.can_edit', raise_exception=True)
@@ -26,13 +26,13 @@ def edit_book(request, pk):
         book.title = request.POST.get("title")
         book.author = request.POST.get("author")
         book.save()
-        return redirect("list_books")
+        return redirect("book_lists")
     return render(request, "books/edit_book.html", {"book": book})
 
 @permission_required('books.can_delete', raise_exception=True)
 def delete_book(request, pk):
     book = get_object_or_404(Book, pk=pk)
     book.delete()
-    return redirect("list_books")
+    return redirect("book_lists")
 
 # Create your views here.
