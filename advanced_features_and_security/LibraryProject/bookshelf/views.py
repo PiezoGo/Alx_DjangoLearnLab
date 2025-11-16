8ab3117d381a01fc7ae0f4f8d8b04b188707dafd
PiezoGo <Piezo5go@gmail.com>
2025-11-16 21:32:31 +0300

    sjsjsjs

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render
 
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import permission_required
-from .models import Book
+from .models import Book 
+from .forms import ExampleForm
 
 @permission_required('books.can_view', raise_exception=True)
 def book_lists(request):
