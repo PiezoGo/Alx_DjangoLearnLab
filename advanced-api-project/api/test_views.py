f517fc78ea5ff4e3bd3e81efdd669e741237ed8c
Piezo <piezo@localhost.localdomain>
2025-11-30 19:19:00 +0300

    Final

--- advanced-api-project/api/test_views.py
+++ advanced-api-project/api/test_views.py
@@ -30,7 +30,7 @@ class BookAPITestCase(TestCase):
         self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
 
     def test_create_book_authenticated(self):
-        self.client.force_authenticate(user=self.user)
+        self.client.login(username='testuser', password='testpass')
         response = self.client.post('/api/books/create/', self.book_data)
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
@@ -40,7 +40,7 @@ class BookAPITestCase(TestCase):
         self.assertEqual(response.data['title'], 'Test Book')
 
     def test_update_book_owner(self):
-        self.client.force_authenticate(user=self.user)
+        self.client.login(username='testuser', password='testpass')
         update_data = {'title': 'Updated Book'}
         response = self.client.patch(f'/api/books/{self.book.id}/update/', update_data)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
@@ -48,14 +48,14 @@ class BookAPITestCase(TestCase):
         self.assertEqual(self.book.title, 'Updated Book')
 
     def test_update_book_non_owner(self):
-        other_user = User.objects.create_user(username='other', password='otherpass')
-        self.client.force_authenticate(user=other_user)
+        User.objects.create_user(username='other', password='otherpass')
+        self.client.login(username='other', password='otherpass')
         update_data = {'title': 'Hacked Book'}
         response = self.client.patch(f'/api/books/{self.book.id}/update/', update_data)
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
 
     def test_delete_book_owner(self):
-        self.client.force_authenticate(user=self.user)
+        self.client.login(username='testuser', password='testpass')
         response = self.client.delete(f'/api/books/{self.book.id}/delete/')
         self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
         self.assertFalse(Book.objects.filter(id=self.book.id).exists())
@@ -85,7 +85,7 @@ class BookAPITestCase(TestCase):
         self.assertEqual(response.data[0]['title'], 'Another Book')
 
     def test_user_books_endpoint(self):
-        self.client.force_authenticate(user=self.user)
+        self.client.login(username='testuser', password='testpass')
         response = self.client.get('/api/books/my-books/')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(len(response.data), 1)
