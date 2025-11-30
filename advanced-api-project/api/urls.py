129fc740f5c872d8aa7a2836a34132825f91329f
Piezo <piezo@localhost.localdomain>
2025-11-30 19:00:58 +0300

    This is a new project. Polishing my DRF API skills

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -4,7 +4,7 @@ from .views import ListView,DeleteView,DetailView,UpdateView,CreateView
 urlpatterns = [
     path('books/', ListView.as_view(), name='list'),
     path('books/<int:pk>', DetailView.as_view(), name='details'),
-    path('books/<int:pk>/update', UpdateView.as_view(), name='update'),
-    path('books/<int:pk>/delete', DeleteView.as_view(), name='delete'),
-    path('books/new', CreateView.as_view(), name='new_book'),
+    path('books/update', UpdateView.as_view(), name='update'),
+    path('books/delete', DeleteView.as_view(), name='delete'),
+    path('books/create', CreateView.as_view(), name='new_book'),
 ]
