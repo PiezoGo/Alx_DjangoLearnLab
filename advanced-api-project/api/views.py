1ea75da80b7039f54c35502c7b6152fd0d221d83
Piezo <piezo@localhost.localdomain>
2025-11-30 19:13:34 +0300

    This is a new project. Polishing my DRF API skills

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,4 +1,5 @@
 from rest_framework import generics, filters
+from django_filters import rest_framework as django_filters
 from .models import Book
 from .serializers import BookSerializer
 from . import permissions
@@ -7,43 +8,23 @@ class ListView(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer  
     permission_classes = [permissions.AllowAny]
-    filter_backends = [filters.SearchFilter, filters.OrderingFilter]
-    search_fields = ['title', 'author', 'description']
+    
+    # Required filter backends
+    filter_backends = [
+        django_filters.DjangoFilterBackend,  # For filtering
+        filters.SearchFilter,                # For searching  
+        filters.OrderingFilter               # For ordering
+    ]
+    
+    # Filtering by exact attributes
+    filterset_fields = ['title', 'author', 'publication_year']
+    
+    # Search functionality
+    search_fields = ['title', 'author']
+    
+    # Ordering
     ordering_fields = ['title', 'author', 'publication_year', 'price']
     ordering = ['-created_at']
-
-    def get_queryset(self):
-        queryset = super().get_queryset()
-        
-        author = self.request.query_params.get('author')
-        if author:
-            queryset = queryset.filter(author__icontains=author)
-        
-        genre = self.request.query_params.get('genre')
-        if genre:
-            queryset = queryset.filter(genre=genre)
-        
-        year = self.request.query_params.get('publication_year')
-        if year:
-            queryset = queryset.filter(publication_year=year)
-        
-        year_min = self.request.query_params.get('publication_year_min')
-        if year_min:
-            queryset = queryset.filter(publication_year__gte=year_min)
-        
-        year_max = self.request.query_params.get('publication_year_max')
-        if year_max:
-            queryset = queryset.filter(publication_year__lte=year_max)
-        
-        price_min = self.request.query_params.get('price_min')
-        if price_min:
-            queryset = queryset.filter(price__gte=float(price_min))
-        
-        price_max = self.request.query_params.get('price_max')
-        if price_max:
-            queryset = queryset.filter(price__lte=float(price_max))
-        
-        return queryset
 
 class UserBookListView(generics.ListAPIView):
     serializer_class = BookSerializer
