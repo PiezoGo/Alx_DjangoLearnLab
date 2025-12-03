6dbb970170a374ee38401edbdf585d7ff7a68ae9
Piezo <piezo@localhost.localdomain>
2025-12-03 10:24:38 +0300

    I hav added the dafault user model provided by django

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -1,9 +1,6 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
-class User(models.Model):
-    name = models.CharField(max_length=30)
-    email = models.EmailField()
 
 
 class Post(models.Model):
