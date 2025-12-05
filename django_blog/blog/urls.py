93ba2584b8b89f9357e6e3a2b3a66c721679f2c5
PiezoTab <Piezo5go@gmail.com>
2025-12-05 07:58:09 +0000

    try

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -3,5 +3,5 @@ from django.urls import path,include
 urlpatterns = [
      path('accounts/', include('django.contrib.auth.urls')),
     path('accounts/profile/',TemplateView.as_view(template_name='accounts/profile.html'),name='profile'),
-    path("signup/", SignUpView.as_view(), name="templates/registration/signup"),
+    path("register/", RegisterView.as_view(), name="templates/registration/register"),
     ]
